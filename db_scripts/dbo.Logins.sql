4665e957930e1c905ddeee264b9740ce65dd52fd
andranick <andranick@bk.ru>
2017-06-24 20:17:32 +0300

    добалены поля

--- db_scripts/dbo.Logins.sql
+++ db_scripts/dbo.Logins.sql
@@ -7,12 +7,16 @@ CREATE TABLE dbo.Logins
 	LoginEmail           nvarchar(64)  NOT NULL ,
 	LoginRoleID          tinyint  NOT NULL ,
 	IsConfirmed          bit  NOT NULL ,
+	ShowNick			 BIT  NOT NULL,
+	LoginView			 AS (case when[ShowNick]= (0) OR [LoginNickName] IS NULL then [LoginName] else [LoginNickName] end) PERSISTED,	
 	CONSTRAINT PK_Logins PRIMARY KEY  CLUSTERED (LoginID ASC),
 	CONSTRAINT FK_Logins_LoginRoles FOREIGN KEY (LoginRoleID) REFERENCES dbo.LoginRoles(LoginRoleID)
 )
 GO
 
 ALTER TABLE [dbo].[Logins] ADD  CONSTRAINT [DF_Logins_IsConfirmed]  DEFAULT ((0)) FOR [IsConfirmed]
+GO
+ALTER TABLE [dbo].[Logins] ADD  CONSTRAINT [DF_Logins_ShowNick]  DEFAULT ((0)) FOR [ShowNick]
 GO
 
 
@@ -21,9 +25,11 @@ INSERT INTO dbo.Logins(
 	LoginEmail,
 	LoginNickName,
 	LoginRoleID,
-	LoginPassword
+	LoginPassword,
+	IsConfirmed,
+	ShowNick
 )
-VALUES('@andranick@bk.ru', '@andranick@bk.ru', 'andranick', 1, '1',1);
+VALUES('@andranick@bk.ru', '@andranick@bk.ru', 'andranick', 1, '1',1,1);
 GO
 
 	
