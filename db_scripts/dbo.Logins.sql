d04ac0825b68893109de8c0e2cd06ce2697475cb
andranick <andranick@bk.ru>
2017-07-01 12:23:09 +0300

    таблица dbo.Logins

--- db_scripts/dbo.Logins.sql
+++ db_scripts/dbo.Logins.sql
@@ -1,14 +1,14 @@
 CREATE TABLE dbo.Logins
 ( 
-	LoginID              bigint IDENTITY ( 1,1 ) ,
-	LoginName            nvarchar(64)  NOT NULL ,
-	LoginNickName        nvarchar(32)  NULL ,
-	LoginPassword        nvarchar(128)  NOT NULL ,
-	LoginEmail           nvarchar(64)  NOT NULL ,
-	LoginRoleID          tinyint  NOT NULL ,
-	IsConfirmed          bit  NOT NULL ,
-	ShowNick			 BIT  NOT NULL,
-	LoginView			 AS (case when[ShowNick]= (0) OR [LoginNickName] IS NULL then [LoginName] else [LoginNickName] end) PERSISTED,	
+	LoginID             bigint IDENTITY ( 1,1 ) ,
+	LoginRoleID         tinyint  NOT NULL ,
+	LoginName			nvarchar(32)  NOT NULL ,
+	[Password]			nvarchar(128)  NOT NULL ,
+	Email				nvarchar(64)  NOT NULL ,
+	IsConfirmed         bit  NOT NULL ,
+	Name				nvarchar(64) NULL ,
+	ShowName			BIT  NOT NULL,
+	LoginView			AS (case when[ShowName]= (0) OR [Name] IS NULL then [LoginName] else [Name] end) PERSISTED,	
 	CONSTRAINT PK_Logins PRIMARY KEY  CLUSTERED (LoginID ASC),
 	CONSTRAINT FK_Logins_LoginRoles FOREIGN KEY (LoginRoleID) REFERENCES dbo.LoginRoles(LoginRoleID)
 )
@@ -16,20 +16,20 @@ GO
 
 ALTER TABLE [dbo].[Logins] ADD  CONSTRAINT [DF_Logins_IsConfirmed]  DEFAULT ((0)) FOR [IsConfirmed]
 GO
-ALTER TABLE [dbo].[Logins] ADD  CONSTRAINT [DF_Logins_ShowNick]  DEFAULT ((0)) FOR [ShowNick]
+ALTER TABLE [dbo].[Logins] ADD  CONSTRAINT [DF_Logins_ShowNick]  DEFAULT ((0)) FOR [ShowName]
 GO
 
 
 INSERT INTO dbo.Logins(
-	LoginName,
-	LoginEmail,
-	LoginNickName,
 	LoginRoleID,
-	LoginPassword,
+	LoginName,
+	[Password],
+	Email,
+	Name,	
 	IsConfirmed,
-	ShowNick
+	ShowName
 )
-VALUES('@andranick@bk.ru', '@andranick@bk.ru', 'andranick', 1, '1',1,1);
+VALUES(1,'andranick','1','@andranick@bk.ru', 'Андрей Багиров', 1,0);
 GO
 
 	
