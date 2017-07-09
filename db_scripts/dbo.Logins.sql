9319e24b7fbf01073d3e6afb1fb7e5e6b1ab9514
andranick <andranick@bk.ru>
2017-07-09 18:42:53 +0300

    коррекция структуры

--- db_scripts/dbo.Logins.sql
+++ db_scripts/dbo.Logins.sql
@@ -6,9 +6,9 @@ CREATE TABLE dbo.Logins
 	[Password]			nvarchar(128)  NOT NULL ,
 	Email				nvarchar(64)  NOT NULL ,
 	IsConfirmed         bit  NOT NULL ,
-	Name				nvarchar(64) NULL ,
-	ShowName			BIT  NOT NULL,
-	LoginView			AS (case when[ShowName]= (0) OR [Name] IS NULL then [LoginName] else [Name] end) PERSISTED,	
+	ScreenName			nvarchar(64) NULL ,
+	ShowScreenName		BIT  NOT NULL,
+	LoginView			AS (case when[ShowScreenName]= (0) OR [ScreenName] IS NULL then [LoginName] else [ScreenName] end) PERSISTED,	
 	CONSTRAINT PK_Logins PRIMARY KEY  CLUSTERED (LoginID ASC),
 	CONSTRAINT FK_Logins_LoginRoles FOREIGN KEY (LoginRoleID) REFERENCES dbo.LoginRoles(LoginRoleID)
 )
@@ -16,7 +16,7 @@ GO
 
 ALTER TABLE [dbo].[Logins] ADD  CONSTRAINT [DF_Logins_IsConfirmed]  DEFAULT ((0)) FOR [IsConfirmed]
 GO
-ALTER TABLE [dbo].[Logins] ADD  CONSTRAINT [DF_Logins_ShowName]  DEFAULT ((0)) FOR [ShowName]
+ALTER TABLE [dbo].[Logins] ADD  CONSTRAINT [DF_Logins_ShowScreenName]  DEFAULT ((0)) FOR [ShowScreenName]
 GO
 
 
@@ -25,9 +25,9 @@ INSERT INTO dbo.Logins(
 	LoginName,
 	[Password],
 	Email,
-	Name,	
+	ScreenName,	
 	IsConfirmed,
-	ShowName
+	ShowScreenName
 )
 VALUES(1,'noteplot','1','noteplot@bk.ru', 'Вася Иванов', 1,0);
 GO
