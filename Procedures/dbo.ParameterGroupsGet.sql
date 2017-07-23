b56c27a1788dd4e8eabdf8384ea7cda02b9be833
andranick <andranick@bk.ru>
2017-07-23 18:40:15 +0300

    dbo.ParameterGroups + процедуры

---
+++ Procedures/dbo.ParameterGroupsGet.sql
@@ -0,0 +1,22 @@
+-- ============================================================
+-- Author:		[ab]
+-- Create date: 20170723
+-- Description:	Процедура получения списка групп параметров
+-- ============================================================
+
+IF OBJECT_ID('[dbo].[ParameterGroupsGet]', 'P') is null
+ EXEC('create procedure [dbo].[ParameterGroupsGet] as begin return -1 end')
+GO
+
+ALTER PROCEDURE dbo.ParameterGroupsGet 
+AS
+BEGIN
+	SET NOCOUNT ON;
+	SELECT 
+		ParameterGroupID,
+		ParameterGroupShortName,				
+		ParameterGroupName,
+		LoginID
+	FROM dbo.ParameterGroups
+END	
+GO
---
+++ Tables/dbo.ParameterGroups.sql
@@ -0,0 +1,27 @@
+
+CREATE TABLE dbo.ParameterGroups
+( 
+	ParameterGroupID     bigint IDENTITY ( 1,1 ) ,
+	ParameterGroupShortName nvarchar(24)  NOT NULL ,
+	ParameterGroupName   nvarchar(48)  NOT NULL ,
+	LoginID              bigint  NULL ,
+	CONSTRAINT PK_ParamGroups PRIMARY KEY  CLUSTERED (ParameterGroupID ASC)
+)
+go
+
+
+
+CREATE UNIQUE NONCLUSTERED INDEX IX_ParamGroups_Login_ShortName ON dbo.ParameterGroups
+( 
+	LoginID               ASC,
+	ParameterGroupShortName  ASC
+)
+go
+
+
+
+
+ALTER TABLE dbo.ParameterGroups
+	ADD CONSTRAINT FK_ParameterGroups_Logins FOREIGN KEY (LoginID) REFERENCES dbo.Logins(LoginID)
+go
+
