f0d3975a7b0570f2e427ddbc584f12c935f5024c
andranick <andranick@bk.ru>
2017-09-03 21:47:34 +0300

    коррекция

--- Procedures/dbo.ParamValueTypesGet.sql
+++ Procedures/dbo.ParamValueTypesGet.sql
@@ -2,13 +2,15 @@
 -- Author:		[ab]
 -- Create date: 20170716
 -- Description:	Процедура получения списка типов значений
+-- только для числовых типов
 -- =============================================
 
 IF OBJECT_ID('[dbo].[ParamValueTypesGet]', 'P') is null
  EXEC('create procedure [dbo].[ParamValueTypesGet] as begin return -1 end')
 GO
 
-ALTER PROCEDURE dbo.ParamValueTypesGet 
+ALTER PROCEDURE dbo.ParamValueTypesGet
+@IsNumeric BIT = 1 
 AS
 BEGIN
 	SET NOCOUNT ON;
@@ -21,5 +23,6 @@ BEGIN
 		[Scale],
 		[Precision]
 	FROM dbo.ParamValueTypes
+	WHERE [IsNumeric] = IsNull(@IsNumeric,[IsNumeric])
 END
 GO
