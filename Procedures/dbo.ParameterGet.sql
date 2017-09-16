6a4df0553449ee770fc43b6ab50c81a90b3b76ca
andranick <andranick@bk.ru>
2017-09-16 23:00:44 +0300

    Процедура ParameterGet

---
+++ Procedures/dbo.ParameterGet.sql
@@ -0,0 +1,42 @@
+-- ============================================================
+-- Author:		[ab]
+-- Create date: 20170916
+-- Description:	Процедура получения списка параметров или параметра
+-- ============================================================
+
+IF OBJECT_ID('[dbo].[ParameterGet]', 'P') is null
+ EXEC('create procedure [dbo].[ParameterGet] as begin return -1 end')
+GO
+
+ALTER PROCEDURE dbo.ParameterGet
+@ParameterID BIGINT = null,
+@LoginID	BIGINT 
+AS
+BEGIN
+	SET NOCOUNT ON;
+	SELECT 
+		ps.ParameterID					AS ParameterID,
+		ps.ParameterGroupID				AS ParameterGroupID,
+		pg.ParameterGroupShortName		AS ParameterGroupShortName,
+		p.ParamShortName				AS ParameterShortName,
+		p.ParamName						AS ParametertName,
+		p.ParamUnitID					AS ParameterUnitID,
+		u.UnitShortName					AS ParameterUnitShortName,
+		p.ParamTypeID					AS ParameterTypeID,
+		pt.ParamTypeName				AS ParameterTypeName,
+		p.ParamValueTypeID				AS ParameterValueTypeID,
+		pvt.ParamValueTypeShortName		AS ParameterValueTypeShortName,
+		p.ParamValueMAX					AS ParameterValueMAX,
+		p.ParamValueMIN					AS ParameterValueMIN,
+		p.LoginID						AS LoginID
+	FROM dbo.Params AS p
+	JOIN dbo.Parameters AS ps ON ps.ParameterID = p.ParamID
+	JOIN dbo.ParameterGroups AS pg ON pg.ParameterGroupID = ps.ParameterGroupID
+	JOIN dbo.Units AS u ON u.UnitID = p.ParamUnitID
+	JOIN dbo.ParamTypes AS pt ON pt.ParamTypeID = p.ParamTypeID
+	JOIN dbo.ParamValueTypes AS pvt ON pvt.ParamValueTypeID = p.ParamValueTypeID 	
+	WHERE 
+	ps.ParameterID = IsNull(@ParameterID,ps.ParameterID) and
+	p.LoginID IN (0,@LoginID) 
+END	
+GO
