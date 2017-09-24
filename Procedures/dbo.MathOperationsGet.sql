5941358a883799c356ecb9ceab886d0f93997ebf
andranick <andranick@bk.ru>
2017-09-24 20:57:54 +0300

    Типы операций над параметрами

---
+++ Procedures/dbo.MathOperationsGet.sql
@@ -0,0 +1,22 @@
+-- ============================================================
+-- Author:		[ab]
+-- Create date: 20170924
+-- Description:	Процедура получения списка опрераций с параметрами
+-- ============================================================
+
+IF OBJECT_ID('[dbo].[MathOperationsGet]', 'P') is null
+ EXEC('create procedure [dbo].[MathOperationsGet] as begin return -1 end')
+GO
+
+ALTER PROCEDURE dbo.MathOperationsGet 
+AS
+BEGIN
+	SET NOCOUNT ON;
+	SELECT 
+		mo.MathOperationID,
+		mo.MathOperationShortName,
+		mo.MathOperationName,
+		mo.MathOperationShortName + ' ('+mo.MathOperationName+')' AS MathOperationFullName  
+	FROM dbo.MathOperations AS mo
+END	
+GO
