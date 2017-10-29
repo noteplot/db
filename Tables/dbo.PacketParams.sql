7eda226e527a709936f74aaa1db20de08c480cd6
andranick <andranick@bk.ru>
2017-10-29 13:56:18 +0300

    Добавлен признак Active - доступность для выбора - для параметров в пакете

--- Tables/dbo.PacketParams.sql
+++ Tables/dbo.PacketParams.sql
@@ -4,12 +4,12 @@ CREATE TABLE dbo.PacketParams
 	PacketID             bigint  NOT NULL ,
 	ParamID              bigint  NOT NULL ,
 	PacketParamPosition  smallint  NOT NULL ,
+	Active               bit  NOT NULL ,
 	CONSTRAINT PK_PacketParams PRIMARY KEY  CLUSTERED (PacketParamID ASC),
 	CONSTRAINT FK_PacketParams_Params FOREIGN KEY (ParamID) REFERENCES dbo.Params(ParamID),
-	CONSTRAINT FK_PacketParams_Packets FOREIGN KEY (PacketID) REFERENCES dbo.Packets(PacketID)
+CONSTRAINT FK_PacketParams_Packets FOREIGN KEY (PacketID) REFERENCES dbo.Packets(PacketID)
 )
 go
-
 
 
 CREATE UNIQUE NONCLUSTERED INDEX IU_PacketParams_PacketID_ParamID ON dbo.PacketParams
@@ -30,8 +30,8 @@ go
 
 
 ALTER TABLE dbo.PacketParams
-	ADD CONSTRAINT DF_PacketParams_Position
-		 DEFAULT  1 FOR PacketParamPosition
+	ADD CONSTRAINT DF_PacketParams_Active
+		 DEFAULT  1 FOR Active
 go
 
 
