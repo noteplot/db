3cf84af76353451175511b9d9d7b3e4ef2b7aea7
andranick <andranick@bk.ru>
2017-12-09 11:50:52 +0300

    убрал логин из мониторинга

--- Tables/dbo.Monitorings.sql
+++ Tables/dbo.Monitorings.sql
@@ -4,12 +4,10 @@ CREATE TABLE dbo.Monitorings
 	MonitorID            bigint  NOT NULL ,
 	MonitoringDate       DATETIME2(0)  NOT NULL ,
 	MonitoringComment    nvarchar(255)  NULL ,
-	LoginID              bigint  NOT NULL ,
 	CreationDate         datetime  NOT NULL ,
 	ModifiedDate         datetime  NOT NULL ,
 	CONSTRAINT PK_Monitorings PRIMARY KEY  CLUSTERED (MonitoringID ASC),
 	CONSTRAINT FK_Monitorings_Monitors FOREIGN KEY (MonitorID) REFERENCES dbo.Monitors(MonitorID),
-	CONSTRAINT FK_Monitorings_Logins FOREIGN KEY (LoginID) REFERENCES dbo.Logins(LoginID)
 )
 go
 
